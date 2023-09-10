10a0cf9e2353fe5d665d82f519d58086d8247243
Daniel <dan.tgw@gmail.com>
2023-09-10 17:31:38 +0800

    Added matching ratio, deadline, and refund.

--- contracts/MatchingProgram.sol
+++ contracts/MatchingProgram.sol
@@ -4,15 +4,31 @@ contract MatchingProgram {
     uint public state;
     address recipient;
     bytes32 recipientName;
-    uint initialMatchingAmount;
+    address sponsor;
+    bytes32 sponsorName;
+
+    uint matchingAmount;
     uint matchingAmountLeft;
     bytes32 programName;
+    uint matchingRatio;
+    uint endTime;
+
+    mapping(address => uint) donors;
+    uint donorCount;
 
     event DonatedEvent(uint donatedAmount);
     event MatchingProgramStartedEvent(uint matchingAmount);
 
     error NoAmountSent();
+    error PastInitialFundState();
     error NoMatchingAmountSet();
+    error FundraiseEnded();
+    error FundraiseNotEnded();
+    error RatioLessThanOne();
+
+    struct Donation {
+        uint amount;
+    }
 
     // Function to get the current count
     function getState() public view returns (uint) {
@@ -24,43 +40,75 @@ contract MatchingProgram {
     }
 
     function getInitialMatchingAmount() public view returns (uint) {
-        return initialMatchingAmount;
+        return matchingAmount;
     }
 
     constructor(
         bytes32 inputProgramName,
         address intendedRecipient,
-        bytes32 inputRecipientName
+        bytes32 inputRecipientName,
+        address inputSponsor,
+        bytes32 inputSponsorName,
+        uint inputEndTime
     ) {
         recipient = intendedRecipient;
         recipientName = inputRecipientName;
+        sponsor = inputSponsor;
+        sponsorName = inputSponsorName;
         programName = inputProgramName;
+        endTime = inputEndTime;
         state = 0;
+        donorCount = 0;
     }
 
-    function sendMatchingAmount() external payable {
+    function fundInitialMatchingAccount(uint ratio) external payable {
         if (msg.value == 0) revert NoAmountSent();
-
+        if (state != 0) revert PastInitialFundState();
+        if (ratio <= 0) revert RatioLessThanOne();
+        if (block.timestamp < endTime) {
+            revert FundraiseEnded();
+        }
         matchingAmountLeft = msg.value;
-        initialMatchingAmount = msg.value;
-
+        matchingAmount = msg.value;
+        matchingRatio = ratio;
         emit MatchingProgramStartedEvent(matchingAmountLeft);
-
         state = 1;
     }
 
     function donate() external payable {
         // msg.value
         if (state == 0) revert NoMatchingAmountSet();
-        if (msg.value <= matchingAmountLeft) {
-            matchingAmountLeft -= msg.value;
-            payable(recipient).transfer(msg.value + msg.value);
-            emit DonatedEvent(msg.value + msg.value);
-        } else if (msg.value > matchingAmountLeft) {
-            uint amountToSend = msg.value + matchingAmountLeft;
+        if (block.timestamp < endTime) {
+            revert FundraiseEnded();
+        }
+        uint amountToSend = 0;
+        if (msg.value <= matchingRatio * matchingAmountLeft) {
+            amountToSend = msg.value * (matchingRatio + 1);
+            matchingAmountLeft -= msg.value * matchingRatio;
+            payable(recipient).transfer(amountToSend);
+        } else if (msg.value > matchingRatio * matchingAmountLeft) {
+            amountToSend = msg.value + matchingAmountLeft;
             matchingAmountLeft = 0;
             payable(recipient).transfer(amountToSend);
-            emit DonatedEvent(amountToSend);
         }
+        if (donors[msg.sender] == 0) {} else {
+            donorCount += 1;
+        }
+        donors[msg.sender] += msg.value;
+        emit DonatedEvent(amountToSend);
+    }
+
+    function endFundRaise() external {
+        if (block.timestamp < endTime) {
+            revert FundraiseEnded();
+        }
+        state = 2;
+    }
+
+    function refundMatchingAmount() external {
+        if (state < 2) {
+            revert FundraiseNotEnded();
+        }
+        payable(sponsor).transfer(matchingAmountLeft);
     }
 }
