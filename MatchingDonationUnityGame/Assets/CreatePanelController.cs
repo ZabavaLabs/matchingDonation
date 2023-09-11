d112489267424561de718c878fff063a8a4544ed
dantgw <dan.tgw@gmail.com>
2023-09-12 02:07:44 +0800

    Added Nethereum to encode smart contract arguments

--- MatchingDonationUnityGame/Assets/CreatePanelController.cs
+++ MatchingDonationUnityGame/Assets/CreatePanelController.cs
@@ -6,8 +6,13 @@ using MetaMask.Unity;
 using MetaMask.Models;
 using MetaMask.IO;
 using MetaMask.Contracts;
+using Nethereum.ABI;
+using Nethereum.Util;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Hex;
+using Nethereum.Web3;
 
-
+using Nethereum.Hex.HexConvertors.Extensions;
 
 public class CreatePanelController : MonoBehaviour
 {
@@ -43,7 +48,15 @@ public class CreatePanelController : MonoBehaviour
     {
         var wallet = MetaMaskUnity.Instance.Wallet;
 
+        var web3 = new Web3("https://mainnet.infura.io");
+        string abi = "TO BE CHANGED";
+        var contract = web3.Eth.GetContract(abi, "0x3615594d996649741018000670FF6669A4205d83");
+        var function = contract.GetFunction("getCollectibleDetails");
 
+
+        var abiEncode = new ABIEncode();
+        var data = abiEncode.GetABIEncoded(new ABIValue("bytes32", "hello"), new ABIValue("uint", 69),
+                        new ABIValue("string", "world")).ToHex();
 
 
         var transactionParams = new MetaMaskTransaction
@@ -51,7 +64,7 @@ public class CreatePanelController : MonoBehaviour
             To = "0x3615594d996649741018000670FF6669A4205d83",
             From = MetaMaskUnity.Instance.Wallet.SelectedAddress,
             Value = "0x0",
-
+            Data = data
         };
 
         var request = new MetaMaskEthereumRequest
